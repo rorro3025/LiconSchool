5e04fcf7bfdabb5d4f5a3ff3be676311f58cbeeb
RodrigoH <rodrigo.hernandez@licon.com.mx>
2025-06-20 12:10:42 -0600

    Grid no work :(

--- src/componets/Layout/index.tsx
+++ src/componets/Layout/index.tsx
@@ -2,24 +2,33 @@ import { ReactNode } from "react";
 import Navbar from "../Navbar";
 import { Grid } from "@mantine/core";
 import OfflineHeader from "../OfflineHeader";
+import Head from 'next/head'
 
 interface Props {
   children: ReactNode;
+  title?: string
 }
 
-function Layout({ children }: Props) {
+//
+
+function Layout({ children, title }: Props) {
   return (
-    <>
-      <OfflineHeader />
-      <Grid >
-        <Grid.Col span={1}>
-          <Navbar />
-        </Grid.Col >
-        <Grid.Col>
-          {children}
-        </Grid.Col>
-      </Grid>
-    </>
+    <Grid>
+      <Head>
+        <title>{title || 'LiconSchool'}</title>
+        <meta name="description" content="An offline first app using Next.js and Mantine" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Grid.Col>
+        <Navbar />
+        <Grid>
+          <Grid.Col span={12} style={{ padding: "20px" }}>
+            <OfflineHeader />
+            {children}
+          </Grid.Col>
+        </Grid>
+      </Grid.Col>
+    </Grid>
   );
 }
 
